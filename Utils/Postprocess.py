687228e4126e67249183993af773b809983d97f3
curiosity29 <dung.dm195860@sis.hust.edu.vn>
2024-03-14 09:33:23 +0700

    update missed initial commit

--- Utils/Postprocess.py
+++ Utils/Postprocess.py
@@ -2,26 +2,10 @@ import numpy as np
 import cv2
 from scipy.stats import multivariate_normal
 import tensorflow as tf
-def norm_kernel(size = 9):
-  # Define the mean and covariance matrix
-  mean = [0, 0]
-  cov = [[1, 0], [0, 1]]
-
-  # Create a 2D normal distribution
-  rv = multivariate_normal(mean, cov)
-
-  # Generate a 2D array with values following the distribution
-  x = np.linspace(-3, 3, size)
-  y = np.linspace(-3, 3, size)
-  X, Y = np.meshgrid(x, y)
-  Z = rv.pdf(np.dstack((X, Y)))
-  filter = Z - np.mean(Z)
-  # Print the 2D array
-  return filter
-
-def amplify(label, strength = 3):
-  label = label + strength * cv2.filter2D(label, -1, norm_kernel(5)) + strength * cv2.filter2D(label, -1, norm_kernel(9)) #cv2.erode(image, np.ones(9,9),iterations = 1 )
-  return label
+import shapely.geometry as geometry
+from shapely import Polygon
+from rasterstats import zonal_stats
+import geopandas as gd
    
 def predict_adapter(batch, model, strength):
   ## dilated predict
@@ -32,5 +16,204 @@ def predict_adapter(batch, model, strength):
   predict = predict[..., np.newaxis]
 
   return predict
+
+## simplify polygon:
+def filter_polygons(pathShape, pathMask):
+
+  stats = zonal_stats(pathShape, pathMask,
+            # stats="count min mean max median")
+            stats="mean")
   
+  gdf_filtered = gd.read_file(pathShape)
+
+  farm_threshold = 0.4
+  remove_list = []
+  for idx in range(len(stats)):
+    if stats[idx]['mean'] is not None and stats[idx]["mean"] < farm_threshold:
+      remove_list.append(idx)
+  len(remove_list)
   
+  gdf_filtered = gdf_filtered.drop(remove_list)
+  return gdf_filtered
+
+# gdf.drop(remove_list)
+  
+## simplify polygon:
+def simplify_polygons(gdf):
+  simplified_gdf = gdf.copy()
+  # remove_list = []
+  for idx, row in gdf.iterrows():
+    geom = row["geometry"]
+    area = geom.area
+    tolerance = min(1., np.sqrt(area) /4)
+    simple_geom = geom.simplify(tolerance, preserve_topology=False)
+    # if simple_geom.area < eps:
+    #   remove_list.append(idx)
+    #   continue
+    row["geometry"] = simple_geom
+    # print(row["geometry"])
+    simplified_gdf.loc[idx] = row
+  
+  return simplified_gdf
+
+
+def get_angle(pt0, pt1, pt2):
+    """
+    Calculates the angle between three points.
+
+    Args:
+        pt1: The first point.
+        pt2: The second point.
+        pt0: The reference point.
+
+    Returns:
+        The angle in degrees.
+    """
+
+    # Calculate the vectors from the reference point to the other two points.
+    v1 = np.array(pt1) - np.array(pt0)
+    v2 = np.array(pt2) - np.array(pt0)
+
+    # Calculate the angle between the vectors.
+    angle = np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
+
+    # Convert the angle to degrees.
+    angle = np.degrees(angle)
+
+    # Return the angle.
+    return angle
+
+
+def checkAngle(concave_set, coords, threshold = 30):
+  n = len(concave_set)
+  start = concave_set[0]
+  end = concave_set[n-1]
+  # get 3 point to calculate angle
+  if start == 0:
+    idx1 = -1
+  else:
+    idx1 = start - 1
+  if end == n - 1:
+    idx2 = 0
+  else:
+    idx2 = end + 1
+
+  sharp_indexs = []
+  for idx0 in concave_set:
+
+    pt0, pt1, pt2 = coords[idx0], coords[idx1], coords[idx2]
+    angle = get_angle(pt0, pt1, pt2)
+    # return angle
+
+    if angle < threshold:
+      sharp_indexs.append(idx0)
+
+  return sharp_indexs
+
+def refine_polygon(geom):
+  """
+  connect slender path (currently not implemented):
+        get distance to extended cut
+        get distance to mid point of cvhull dc
+        if dedges > k * dc:
+          extends
+          create 2 new polygon
+          recursion
+        else:
+          remove point
+          recursion
+
+  """
+
+  # simple_geom = geom.simplify(4., preserve_topology=False)
+  convexHull = geom.convex_hull
+  geom_coords = list(geom.exterior.coords)
+
+  convexHull_coords = list(convexHull.exterior.coords)
+  concave_sets = []
+  current_concave = False
+  current_set = []
+  for idx, point in enumerate(geom_coords):
+      if point not in convexHull_coords:
+        if current_concave:
+          current_set.append(idx)
+        else:
+          concave_sets.append(current_set)
+          current_concave = False
+          
+  sharp_indexs = []
+  for set_ in concave_sets:
+    remove_indexs = checkAngle(set_, geom_coords)
+    for index_ in remove_indexs:
+      sharp_indexs.append(index_)
+
+
+  keep_coords = []
+  for idx, coord in enumerate(geom_coords):
+    if idx not in sharp_indexs:
+      keep_coords.append(coord)
+
+  polygon = Polygon(keep_coords)
+
+
+
+
+
+  # print(len(sharp_indexs))
+  # if len(keep_coords) != len(geom_coords):
+  #   x, y = geom.exterior.xy
+  #   plt.subplot(121)
+  #   plt.plot(x, y)
+  #   x, y = polygon.exterior.xy
+  #   plt.subplot(122)
+  #   plt.plot(x, y)
+  #   plt.show()
+
+
+
+  return polygon
+
+def refine_polygons(gdf, save_path = None):
+  """
+    save path = None mean no saving and return result
+  """
+  refined_gdf = gdf.copy()
+  row_remove_list = []
+  for idx, row in gdf.iterrows():
+
+    eps = 0.1
+    geom = row["geometry"]
+
+    if geom.geom_type == "MultiPolygon":
+      # print(geom)
+      geoms = list(geom.geoms)
+      polygons = []
+      for geom in geoms:
+        if geom.area < eps:
+            row_remove_list.append(idx)
+            continue
+        polygons.append(refine_polygon(geom))
+      polygon = geometry.MultiPolygon(polygons)
+
+
+    else:
+      ###
+      # continue
+      ###
+      if geom.area < eps:
+        row_remove_list.append(idx)
+        continue
+
+      polygon = refine_polygon(geom)
+
+
+    row["geometry"] = polygon
+
+    refined_gdf.loc[idx] = row
+
+  refined_gdf = refined_gdf.drop(row_remove_list)
+  if save_path is None:
+    return refined_gdf
+
+  else:
+    refined_gdf.to_file(save_path)
--- main.py
+++ main.py
@@ -1,53 +1,80 @@
 import numpy as np
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from functools import partial
-import sys, glob
+import os, sys, glob
 
 from Utils.Window import predict_windows
 from Utils import Preprocess
-from Utils.Postprocess import predict_adapter
-from Model import MainModel
+from Utils.Rasterize import rasterize
+from Utils.Postprocess import predict_adapter_boundary, predict_adapter_farm, filter_polygons, simplify_polygons, refine_polygons
+from Model import U2Net
 import Configs
+import tensorflow as tf
 
 def get_main_args():
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
     arg = parser.add_argument
-    arg("--weight_path", type=str, default="./Checkpoint_weights/*.h5", help="weights.h5 file")
-    arg("--image_path", type=str, default="./Images/*.tif", help="4 channel input tif file")
-    arg("--save_path", type=str, default="./Predictions/prediction.tif", help="1 channel output tif file")
-    arg("--batch_size", type=int, default=4, help="batch size each predict, lowering to reduce memory requirement")
-    arg("--strength", type=float, default=3.0, help="postprocessing parameter, amplify signal for small region")
+    arg("--weight_path_boundary", type=str, default="./Checkpoint_weights/*.h5", help="checkpoint file to create boundary mask")
+    arg("--weight_path_farm", type=str, default="./Checkpoint_weights/*.h5", help="checkpoint file to create farm mask")
 
+    arg("--image_path", type=str, default="./Images/*.tif", help="4 channel input tif file")
+    arg("--save_path_folder", type=str, default="./Predictions/", help="folder to save prediction and other processing file")
+    arg("--batch_size", type=int, default=4, help="batch size each predict, lowering to reduce memory requirement")
+    arg("--boundary_threshold", type=float, default=0.5, help="threshold to create boundary mask from prediction")
+    arg("--farm_threshold", type=float, default=0.5, help="threshold to filter out non-farm polygon from vectorized predicton")
+    arg("--simplify_distance", type=float, default=1.0, help="tolerance to simplify farm boundary polygon")
+    arg("--sharp_angle", type=float, default=30, help="minimum accepted angle (in degree) to exists inside polygon's convex hull, correspongding vertex is removed otherwise")
     return parser.parse_args()
 
 
-def predict(image_path = "./image.tif", save_path = "./prediction.tif", weight_path = "./checkpoint.weights.h5", 
-            batch_size = 4, strength = 3, search_path = True):
-    if search_path:
-        try:
-            weight_path = glob.glob(f"{weight_path}", recursive=True)[0]
-            image_path = glob.glob(f"{image_path}", recursive=True)[0]
-        except:
-            print("image or weight not found")
-            return
+def predict(image_path = "./image.tif", save_path_folder = "./Predictions", 
+            weight_path_boundary = "./Checkpoint_weights/*.weights.h5", weight_path_farm = "./Checkpoint_weights/*.weights.h5",
+            batch_size = 4, simplify_distance = 1., boundary_threshold = 0.5, farm_threshold = 0.5,
+            search_path = True):
 
-    lows, highs = Configs.preprocess_get_bound()
-    preprocess = partial(Preprocess.preprocess, lows = lows, highs = highs)
-    input_dim = 4
+    # if search_path:
+    #     try:
+    #         weight_path = glob.glob(f"{weight_path}", recursive=True)[0]
+    #         image_path = glob.glob(f"{image_path}", recursive=True)[0]
+    #     except:
+    #         print("image or weight not found")
+    #         return
+
+    ### get boundary mask
+    preprocess = lambda x: x/255
+    input_dim = 3
     predict_dim = 1
-    args = Configs.model_get_args()
-    
-    def get_model(weight_path, args):
-        model = MainModel.U2Net_dilated(**args)
-        model.load_weights(weight_path)
-        return model
 
-    model = get_model(weight_path, args)
-    predictor = partial(predict_adapter, model = model, strength = strength)
+    boundary_model = tf.keras.models.load_model(weight_path_boundary)
+    predictor = partial(predict_adapter_boundary, model = boundary_model)
 
-    predict_windows(pathTif = image_path, pathSave = save_path, predictor = predictor, preprocess = preprocess,
+    boundary_mask_path = os.path.join(save_path_folder, "raw_boundary.tif")
+    predict_windows(pathTif = image_path, pathSave = boundary_mask_path, predictor = predictor, preprocess = preprocess,
                     window_size = 512, input_dim = input_dim, predict_dim = predict_dim,
-                    output_type = "int8", batch_size = batch_size)
+                    output_type = "float32", batch_size = batch_size)
+
+
+    ### get farm mask
+
+    args = Configs.model_get_args()
+    farm_model = U2Net(**args)
+    farm_model.load_weights(weight_path_farm)
+    predictor = partial(predict_adapter_farm, model = farm_model)
+
+    farm_mask_path = os.path.join(save_path_folder, "raw_farm.tif")
+    predict_windows(pathTif = image_path, pathSave = farm_mask_path, predictor = predictor, preprocess = preprocess,
+                    window_size = 512, input_dim = input_dim, predict_dim = predict_dim,
+                    output_type = "float32", batch_size = batch_size)
+
+    boundary_shape_path =  os.path.join(save_path_folder, "shape_boundary.shp")
+    rasterize(path_in = boundary_mask_path, path_out = boundary_shape_path)
+    
+    gdf = filter_polygons(boundary_shape_path, farm_mask_path)
+    gdf = simplify_polygons(gdf)
+
+    result_path = os.path.join(save_path_folder, "final_prediction.tif")
+    refine_polygons(gdf, save_path = result_path)
+
 
 if __name__ == "__main__":
     main_args = get_main_args()
